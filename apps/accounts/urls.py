a17445fe6bf10c446f1c72f599cbe5611b7765a2
morteza rasooli <motteza1182@gmail.com>
2026-06-24 18:21:16 +0330

    add logout view

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', views.LoginView, name='login'),
     path('register/', views.RegisterView, name='register'),
+    path('logout/', views.LogoutView, name='logout'),
 ]
